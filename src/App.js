27bd7141f46e55d8c1fbbb05a61b86d9627abb86
dblu <95551108+ddiablou@users.noreply.github.com>
2022-06-24 16:07:12 +0200

    prettier and better practices(?

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import { randomFumo } from 'fumo-api'
+import "./App.css";
+import { useEffect } from "react";
+import { randomFumo } from "fumo-api";
 function App() {
-  const [fumo, setFumo] = useState()
   useEffect(() => {
     async function fetchFumo() {
-    randomFumo().then( (url) => {
-      setFumo(url)
-     })
+      randomFumo().then((url) => {
+        document.getElementById("randomFumoWrapper").src = url
+      });
     }
-    fetchFumo()
-  }, [])
+    fetchFumo();
+  }, []);
+  // Rendering time: Fumos may be slow to render because useEffect hook.
   return (
     <>
-      <div style={{ marginLeft: 'auto', marginRight: 'auto', position: 'relative' }}>
-       <img alt='fumofu' src={fumo} />
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          position: "relative",
+        }}
+      >
+        <img alt="RandomFumo" id="randomFumoWrapper" src={undefined} />
       </div>
     </>
   );
